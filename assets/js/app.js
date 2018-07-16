b7515c0e5a75e3068c9970045d2a1aa5feea176c
kekaduval <32457817+kekaduval@users.noreply.github.com>
2018-07-15 21:03:18 -0400

    whatever

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+        // Transition effect for navbar and back-to-top icon
+        $(window).scrollTo(function() {
+          // checks if window is scrolled more than 500px, adds/removes solid class
+          if($(this).scrollTop() > 550) {
+              $('.navbar').addClass('solid');
+              $('.back-to-top').addClass('hidden');
+          } else {
+              $('.navbar').removeClass('solid');
+              $('.back-to-top').removeClass('hidden');
+          }
+
+        });
